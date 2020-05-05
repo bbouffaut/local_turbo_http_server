cb246ae296a70641a5425adcd0aa433d23479f29
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2020-05-05 23:33:46 +0200

    with callback

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ public_files = __dirname + '/public_files'
 
 const getContentType = function(filePath) {
   var extname = path.extname(filePath);
-  var contentType = 'text/html';
+
   switch (extname) {
+      case '.html':
+          contentType = 'text/html';
+          break
       case '.js':
           contentType = 'text/javascript';
           break
@@ -25,12 +28,15 @@ const getContentType = function(filePath) {
       case '.wav':
           contentType = 'audio/wav';
           break
+      case '.ico':
+        contentType = 'image/ico';
+        break
   }
   return contentType
 }
 
 const getFilePath = function(url) {
-  let filePath = (url.endsWith('/') ) ? `${public_files}/${url}index.html` : public_files + url
+  let filePath = (url.endsWith('/') ) ? `${public_files}${url}index.html` : public_files + url
   return filePath
 }
 
@@ -41,24 +47,91 @@ const removeIntegrityTagsFromHtmlPages = function(data) {
   return result
 }
 
-const server = turbo.createServer(function (req, res) {
-  let filePath = getFilePath(req.url)
-  let contentType = getContentType(filePath);
+const checkIfFileIsCached = function(filePath, res) {
+  let cachedFilePath = `${filePath}_cached`
+  let contentType = 'text/html'
 
   //DEBUG
-  console.log(filePath, contentType);
-  
+  console.log('checkIfFileIsCached', cachedFilePath)
+
+  fs.readFile(cachedFilePath, function (err,data) {
+    if (err) {
+      processHtmlFile(filePath, res);
+    } else {
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', contentType)
+      res.end(data)      
+
+    }
+
+  });
+}
+
+const processHtmlFile = function(filePath, res) {
+
+  let contentType = 'text/html'
+
+  //DEBUG
+  console.log('processHtmlFile', filePath)
+
   fs.readFile(filePath, function (err,data) {
     if (err) {
       res.statusCode = 404;
       res.end(JSON.stringify(err));
       return;
     }
-    let dataClean = (contentType === 'text/html')? removeIntegrityTagsFromHtmlPages(data) : data
+    let dataClean = removeIntegrityTagsFromHtmlPages(data)
+    cacheFile(filePath, dataClean)
+
     res.statusCode = 200;
     res.setHeader('Content-Type', contentType)
     res.end(dataClean);
+    
   });
+
+}
+
+const cacheFile = function(cacheFile,data) {
+  let cachedFilePath = cacheFile + '_cached'
+
+  //DEBUG
+  console.log('cacheFile', cachedFilePath)
+  
+  fs.writeFile(cachedFilePath, data, (err) => {
+    if (err) {
+      console.log('The file has been saved!');
+    }
+    
+  });
+
+}
+
+const server = turbo.createServer(function (req, res) {
+
+  let filePath = getFilePath(req.url)
+  let contentType = getContentType(filePath)
+
+  if (contentType === 'text/html') {
+    checkIfFileIsCached(filePath, res)
+
+  } else {
+
+    fs.readFile(filePath, function (err,data) {
+      if (err) {
+        res.statusCode = 404;
+        res.end(JSON.stringify(err));
+        return;
+      }
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', contentType)
+      res.end(data);
+
+  })
+
+  }
+
 })
 
 server.listen(8080)
